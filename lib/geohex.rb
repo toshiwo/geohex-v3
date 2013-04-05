77cec6775058c8943fcf1d815584e3ba2e5ef37b
toshiwo <toshiwo@toshiwo.com>
2013-04-06 06:52:12 +0900

    remove instance method in Module

--- lib/geohex.rb
+++ lib/geohex.rb
@@ -2,15 +2,5 @@ require "geohex/version"
 require "geohex/v3"
 
 module Geohex
-  def encode(latitude, longitude, level)
-    Geohex::V3::Zone.encode(latitude, longitude, level)
-  end
-  
-  def decode(code)
-    Geohex::V3::Zone.decode(code)
-  end
-  
-  def getZoneByXY(x, y, level)
-    Geohex::V3::Zone.getZoneByXY(x, y, level)
-  end
+  # Your code goes here...
 end
