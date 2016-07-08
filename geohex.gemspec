bcbf00e61f96305c92972e27dd6c7c26262f8baf
toshiwo <toshiwo@toshiwo.com>
2016-07-08 21:30:57 +0900

    remove a unnecessary rr gem

--- geohex.gemspec
+++ geohex.gemspec
@@ -24,7 +24,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'rake', "~> 10.0.4"
   s.add_development_dependency "rspec", "~> 2.14.1"
-  s.add_development_dependency "rr", "~> 1.2.0"
   s.add_development_dependency "simplecov", "~> 0.11.2"
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,15 +13,3 @@ require 'geohex'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path("./support/**/*.rb", File.dirname(__FILE__))].each {|f| require f}
-
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  # config.mock_with :rspec
-  config.mock_with :rr
-end
