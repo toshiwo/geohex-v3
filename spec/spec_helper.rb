4543ff9af430b30e3f21751722180c727b4a0ffa
toshiwo <toshiwo@toshiwo.com>
2016-07-10 00:46:20 +0900

    change to disable monkey patching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,7 @@ require 'geohex'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path("./support/**/*.rb", File.dirname(__FILE__))].each {|f| require f}
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+end
